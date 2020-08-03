785d985e13273c469a564feb926b85e15b4fe72e
li <769752877@qq.com>
2020-08-04 07:52:19 +0800

    正式环境

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: "/uat/zp/main/", //根路径
+  publicPath: "/zp/main/", //根路径
   outputDir: "dist" //打包的时候生成的一个文件名
   // assetsDir:'mzpy_1',//静态资源目录(js,css,img,fonts)这些文件都可以写里面
 };
